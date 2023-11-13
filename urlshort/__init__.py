9764e082fbf91d098613221979af7538ba716747
kovacsmate88 <kovacsmate321@gmail.com>
2023-11-13 01:30:25 +0100

    fix the import issue

--- urlshort/__init__.py
+++ urlshort/__init__.py
@@ -1,18 +1,13 @@
 from flask import Flask
-import config
+from .config import SECRET_KEY
 
 
 def create_app(test_config=None):
     app = Flask(__name__)
-    app.secret_key = config.SECRET_KEY
+    app.secret_key = SECRET_KEY
 
     from . import urlshort
 
     app.register_blueprint(urlshort.bp)
 
     return app
-
-
-if __name__ == "__main__":
-    app = create_app()
-    app.run(port=5000, debug=True)
