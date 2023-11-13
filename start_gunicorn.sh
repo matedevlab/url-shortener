ce24625ade02309c78ad15e671485be4b74cc39a
kovacsmate88 <kovacsmate321@gmail.com>
2023-11-13 13:49:53 +0100

    create scripts to run gunicorn

---
+++ start_gunicorn.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Check if the virtual environment is already activated
+if [ -z "$VIRTUAL_ENV" ]; then
+    echo "Activating virtual environment"
+    source venv/bin/activate
+else
+    echo "Virtual environment already active"
+fi
+
+# Start Gunicorn with the specified configuration
+gunicorn -w 4 -b 0.0.0.0:8000 "urlshort:create_app()"
