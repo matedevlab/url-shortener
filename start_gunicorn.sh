306e5f66425b47d57b1bba6f51a93c8a76ed96c8
kovacsmate88 <kovacsmate321@gmail.com>
2023-11-14 01:43:33 +0100

    create host variable to display the url

--- start_gunicorn.sh
+++ start_gunicorn.sh
@@ -9,4 +9,12 @@ else
 fi
 
 # Start Gunicorn with the specified configuration
-gunicorn -w 4 -b 0.0.0.0:8000 "urlshort:create_app()"
+gunicorn -w 4 -b 0.0.0.0:8000 "urlshort:create_app()" --daemon
+
+echo "Gunicorn is running in daemon mode"
+
+# Get the host IP
+HOST_IP=$(hostname -I | cut -d' ' -f1)
+
+echo "The application can be accessed at http://$HOST_IP:8000"
+
