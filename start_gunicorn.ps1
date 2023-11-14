32869cd134ff7163661fafaeb2668a5cf1acc4ac
kovacsmate88 <kovacsmate321@gmail.com>
2023-11-14 01:49:52 +0100

    display the url of the application

--- start_gunicorn.ps1
+++ start_gunicorn.ps1
@@ -7,4 +7,11 @@ if (-Not [String]::IsNullOrEmpty($env:VIRTUAL_ENV)) {
 }
 
 # Start Gunicorn with the specified configuration
-gunicorn -w 4 -b 0.0.0.0:8000 "urlshort:create_app()"
+gunicorn -w 4 -b 0.0.0.0:8000 "urlshort:create_app()" --daemon
+
+Write-Host "Gunicorn is running in daemon mode"
+
+# Get the host IP (Windows-specific command)
+$HostIP = Get-NetIPAddress -AddressFamily IPv4 | Where-Object { $_.InterfaceAlias -like "Wi-Fi" -or $_.InterfaceAlias -like "Ethernet" } | Select-Object -First 1 -ExpandProperty IPAddress
+
+Write-Host "The application can be accessed at http://$HostIP:8000"
