f8e10d1058529bc55dd157983c40010d36b7b1ef
Máté Levente Galambos <99624995+matedevlab@users.noreply.github.com>
2025-12-04 02:05:52 +0100

    Package app for editable installs and clean config loading

--- urlshort/__init__.py
+++ urlshort/__init__.py
@@ -1,10 +1,11 @@
 from flask import Flask
-from .config import SECRET_KEY
+
+from . import config
 
 
 def create_app(test_config=None):
-    app = Flask(__name__)
-    app.config.from_mapping(SECRET_KEY=SECRET_KEY)
+    app = Flask(__name__, instance_relative_config=True)
+    app.config.from_object(config)
 
     if test_config is not None:
         app.config.update(test_config)
